52974f0cf58eb753252613638f97f65f6129cc80
Fernanda Ramalho <fernandadosantos021999@gmail.com>
2022-05-11 10:15:17 -0300

    Implementacao do componente star e requisicoes http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { Error404Component } from './error-404/error-404.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavBarComponent, Error404Component],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CourseModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full',
+      },
+      {
+        path: '**',
+        component: Error404Component,
+      },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
